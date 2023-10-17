56bf198f904c7e5640390e34c9d4537aac8dd961
congvt <congvt@metechvn.com>
2023-10-17 21:38:08 +0700

    Tạo base phân trang và AutoMapper

---
+++ src/Ext.Blog.Core/Mappers/AutoMapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Ext.Blog.Core.Domain.Content;
+
+namespace Ext.Blog.Core.Models.Content
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Post, PostDto>();
+            CreateMap<Post, PostInListDto>();
+        }
+    }
+}
---
+++ src/Ext.Blog.Core/Models/Content/PostDto.cs
@@ -0,0 +1,27 @@
+﻿using Ext.Blog.Core.Domain.Content;
+
+namespace Ext.Blog.Core.Models.Content
+{
+    public class PostDto : PostInListDto
+    {
+        public Guid CategoryId { get; set; }
+
+        public string? Content { get; set; }
+
+        public Guid AuthorUserId { get; set; }
+
+        public string? Source { get; set; }
+
+        public string? Tags { get; set; }
+
+        public string? SeoDescription { get; set; }
+
+        public DateTime? DateModified { get; set; }
+
+        public bool IsPaid { get; set; }
+
+        public double RoyaltyAmount { get; set; }
+
+        public PostStatus Status { get; set; }
+    }
+}
