c43c64bbd43cdd5379737d041500f53ce8655c69
congvt <congvt@metechvn.com>
2023-10-31 06:27:39 +0700

    Authentication & Authorization với API JWT Token 1

---
+++ src/Ext.Blog.API/Controllers/AdminApis/AuthsController.cs
@@ -0,0 +1,78 @@
+﻿using Ext.Blog.API.Services;
+using Ext.Blog.Core.Domain.Identity;
+using Ext.Blog.Core.Models.Auth.Requests;
+using Ext.Blog.Core.Models.Auth.Responses;
+using Ext.Blog.Core.SeedWorks.Constants;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Ext.Blog.API.Controllers.AdminApis
+{
+    [Route("api/admin/auth")]
+    [ApiController]
+    public class AuthsController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ITokenService _tokenService;
+        public AuthsController (
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            ITokenService tokenService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuthenticatedResult>> Login([FromBody]LoginRequest request)
+        {
+            //Authentication
+            if (request == null)
+            {
+                return BadRequest("Invalid request");
+            }
+
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            if (user == null || user.IsActive == false || user.LockoutEnabled)
+            {
+                return Unauthorized();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(request.UserName, request.Password, false, true);
+            if (!result.Succeeded)
+            {
+                return Unauthorized();
+            }
+
+            //Authorization
+            var roles = await _userManager.GetRolesAsync(user);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(UserClaims.Id, user.Id.ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(UserClaims.FirstName, user.FirstName),
+                new Claim(UserClaims.Roles, string.Join(";", roles)),
+                //new Claim(UserClaims.Permissions, JsonSerializer.Serialize(permissions)),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            var accessToken = _tokenService.GenerateAccessToken(claims);
+            var refreshToken = _tokenService.GenerateRefreshToken();
+
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiryTime = DateTime.Now.AddDays(30);
+            await _userManager.UpdateAsync(user);
+
+            return Ok(new AuthenticatedResult()
+            {
+                Token = accessToken,
+                RefreshToken = refreshToken
+            });
+        }
+    }
+}
---
+++ src/Ext.Blog.Core/Models/Auth/Requests/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Ext.Blog.Core.Models.Auth.Requests
+{
+    public class LoginRequest
+    {
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+    }
+}
